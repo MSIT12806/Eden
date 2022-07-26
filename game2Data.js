28e938d990a06e8cd8e800a19d9c319814624468
Robin <alegalone@gmail.com>
2022-07-26 10:42:03 +0800

    game2 data

---
+++ game2Data.js
@@ -0,0 +1,290 @@
+ var data = [
+  {
+    "level": "level_1",
+    "question": "我上次走路超過一個小時是在何時？"
+  },
+  {
+    "level": "level_1",
+    "question": "分享一個我收過喜歡的禮物是什麼"
+  },
+  {
+    "level": "level_1",
+    "question": "如果我可以搬家，我會想搬去哪，為什麼？"
+  },
+  {
+    "level": "level_1",
+    "question": "通常我看新聞，會看哪一類型的？"
+  },
+  {
+    "level": "level_1",
+    "question": "我吃過很棒的餐廳或小吃是哪家？"
+  },
+  {
+    "level": "level_1",
+    "question": "我去過的國家有幾個？描述一個不錯的回憶"
+  },
+  {
+    "level": "level_1",
+    "question": "請分享一件發生在我小時後的糗事。"
+  },
+  {
+    "level": "level_1",
+    "question": "請分享曾跟我一起住過的家庭成員的姓名與現在的年紀"
+  },
+  {
+    "level": "level_1",
+    "question": "我現在住哪裡？請分享所有我居住過一個月以上的地點"
+  },
+  {
+    "level": "level_1",
+    "question": "比賽看誰可以單腳站立最久"
+  },
+  {
+    "level": "level_1",
+    "question": "大家一起用雙手在頭部扮演大象"
+  },
+  {
+    "level": "level_1",
+    "question": "大家踮著腳尖在原地轉3圈"
+  },
+  {
+    "level": "level_1",
+    "question": "捏著鼻子做自我介紹30秒"
+  },
+  {
+    "level": "level_1",
+    "question": "大家一起假裝在彈奏不同的樂器"
+  },
+  {
+    "level": "level_1",
+    "question": "請介紹一個我認為很好用的手機APP給大家"
+  },
+  {
+    "level": "level_2",
+    "question": "你猜我的工作最難的部分是什麼？"
+  },
+  {
+    "level": "level_2",
+    "question": "你猜我有可能愛看什麼類型的真人實境秀？"
+  },
+  {
+    "level": "level_2",
+    "question": "剛碰面時，你先注意到我的哪個部分？"
+  },
+  {
+    "level": "level_2",
+    "question": "你猜我收到什麼樣的禮物會很開心？"
+  },
+  {
+    "level": "level_2",
+    "question": "1-10分，你覺得我的書桌有多亂？1是最乾淨、10是超混亂，解釋一下。"
+  },
+  {
+    "level": "level_2",
+    "question": "你認為植物在我的照顧下是茁壯成長還是死亡？解釋一下。"
+  },
+  {
+    "level": "level_2",
+    "question": "你覺得我看起來仁慈溫柔嗎？"
+  },
+  {
+    "level": "level_2",
+    "question": "我看起來偏向是創意型還是分析型？"
+  },
+  {
+    "level": "level_2",
+    "question": "我看起來像一個會紋身的人嗎？"
+  },
+  {
+    "level": "level_2",
+    "question": "從我的鞋子，你對我的認識是什麼？"
+  },
+  {
+    "level": "level_2",
+    "question": "你認為我曾經被解雇過嗎，為什麼？"
+  },
+  {
+    "level": "level_2",
+    "question": "你會如何用三個詞彙，去形容你認為我的類型？"
+  },
+  {
+    "level": "level_2",
+    "question": "你認為我喜歡用通訊軟體跟別人聊天嗎？原因是？"
+  },
+  {
+    "level": "level_2",
+    "question": "你覺得我最有可能花大錢買什麼呢？"
+  },
+  {
+    "level": "level_2",
+    "question": "在我小時候，你猜我想要做什麼？"
+  },
+  {
+    "level": "level_2",
+    "question": "完成這句話：光看你的樣子，我會覺得..."
+  },
+  {
+    "level": "level_2",
+    "question": "你覺得我在學校的時候，受歡迎嗎？解釋一下"
+  },
+  {
+    "level": "level_2",
+    "question": "你覺得我是很遵守交通規則的人嗎？解釋一下"
+  },
+  {
+    "level": "level_2",
+    "question": "你覺得我最常聽到的讚美是什麼？"
+  },
+  {
+    "level": "level_2",
+    "question": "你覺得我容易步入戀情嗎？你怎麼判斷的呢？"
+  },
+  {
+    "level": "level_2",
+    "question": "你認為我在學校里擅長什麼科目？"
+  },
+  {
+    "level": "level_3",
+    "question": "過去的一年，你有什麼快樂的回憶？"
+  },
+  {
+    "level": "level_3",
+    "question": "你所經歷過最嚴重的痛苦是什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "你最早對幸福的記憶是什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "你曾放下的夢想是什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "你媽媽叫什麼名字，她最美的地方是什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "你爸爸叫什麼名字，告訴我一件關於他的事情。"
+  },
+  {
+    "level": "level_3",
+    "question": "你生命中哪部分的影響力，是因你生命中所受的傷害而產生的呢？"
+  },
+  {
+    "level": "level_3",
+    "question": "你的初戀對象的名字是什麼？你喜歡對方的原因是？"
+  },
+  {
+    "level": "level_3",
+    "question": "用一個詞來形容戀愛的感覺，你會怎麼形容？"
+  },
+  {
+    "level": "level_3",
+    "question": "倘若可能，你很渴望在婚姻中得到什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "你會給你生命中的這個階段，下什麼標題呢？"
+  },
+  {
+    "level": "level_3",
+    "question": "你上一次讓自己感到很驚訝的事情是什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "評估自己的心智成熟度，你認為自己現在幾歲？"
+  },
+  {
+    "level": "level_3",
+    "question": "你過去的約會互動經驗，最讓你尷尬的事情是什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "在與異性首次約會時，你試圖想展現的自我形象是什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "目前為止，我的什麼部分有給你好印象？"
+  },
+  {
+    "level": "level_3",
+    "question": "你對什麼人事物充滿熱情？"
+  },
+  {
+    "level": "level_3",
+    "question": "我過去的人生中，有什麼是我後悔沒去做或曾做過的？"
+  },
+  {
+    "level": "level_3",
+    "question": "我希望可以活到幾歲，為什麼？"
+  },
+  {
+    "level": "level_3",
+    "question": "我不想別人問我哪方面的問題？"
+  },
+  {
+    "level": "level_4",
+    "question": "你相信每個人都有自己的使命嗎？如果有，你認為我的是什麼？"
+  },
+  {
+    "level": "level_4",
+    "question": "你認為我最吸引人的特質是什麼，不是外在生理上的？"
+  },
+  {
+    "level": "level_4",
+    "question": "你認為我可以做什麼，能大大改善我的生活？"
+  },
+  {
+    "level": "level_4",
+    "question": "我的什麼部分讓你感到滿特別的？"
+  },
+  {
+    "level": "level_4",
+    "question": "你認為我們重要的相似之處可能是什麼？"
+  },
+  {
+    "level": "level_4",
+    "question": "你認為我可以做什麼，能讓我有好的婚姻？"
+  },
+  {
+    "level": "level_4",
+    "question": "你會如何向陌生人描述我？"
+  },
+  {
+    "level": "level_4",
+    "question": "你認為我現在需要聽到什麼？"
+  },
+  {
+    "level": "level_4",
+    "question": "從我們的談話中，你有得到什麼啟發嗎？"
+  },
+  {
+    "level": "level_4",
+    "question": "如果我要脫離單身，你會給我什麼建議？"
+  },
+  {
+    "level": "level_4",
+    "question": "你在我身上看到了自己的哪些部分？"
+  },
+  {
+    "level": "level_4",
+    "question": "請對我承認某件事情。"
+  },
+  {
+    "level": "level_4",
+    "question": "別人要如何，才能讓你表達自己的脆弱？"
+  },
+  {
+    "level": "level_4",
+    "question": "有什麼事情能讓你覺得離我更近？"
+  },
+  {
+    "level": "level_4",
+    "question": "你建議我應該對什麼放手呢？"
+  },
+  {
+    "level": "Final",
+    "question": "離開前，請寫下你要給對方的讚美、感謝、或祝福，離開後才能去看內容（請手寫於身旁的白紙摺兩折後交給對方）"
+  }
+];
