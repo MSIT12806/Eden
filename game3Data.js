9cb53435fb4892e0005f2edaa9476b3d16046200
Robin <alegalone@gmail.com>
2022-07-26 11:02:25 +0800

    game3 data

---
+++ game3Data.js
@@ -0,0 +1,146 @@
+ var data = [
+  {
+    "advantage": "自信表達",
+    "shortcoming": "堅持己見"
+  },
+  {
+    "advantage": "貫徹始終",
+    "shortcoming": "缺乏應變"
+  },
+  {
+    "advantage": "責任感強",
+    "shortcoming": "工作過勞"
+  },
+  {
+    "advantage": "善於改進",
+    "shortcoming": "喜愛批判"
+  },
+  {
+    "advantage": "懂得自制",
+    "shortcoming": "支配慾高"
+  },
+  {
+    "advantage": "擅長整理",
+    "shortcoming": "難忍雜亂"
+  },
+  {
+    "advantage": "同理他人",
+    "shortcoming": "過度大方"
+  },
+  {
+    "advantage": "待人親切",
+    "shortcoming": "費心討好"
+  },
+  {
+    "advantage": "炒熱氣氛",
+    "shortcoming": "難忍尷尬"
+  },
+  {
+    "advantage": "善於讚賞",
+    "shortcoming": "不說真相"
+  },
+  {
+    "advantage": "以人為重",
+    "shortcoming": "容易吃虧"
+  },
+  {
+    "advantage": "樂於助人",
+    "shortcoming": "不會拒絕"
+  },
+  {
+    "advantage": "待人溫柔",
+    "shortcoming": "內向害羞"
+  },
+  {
+    "advantage": "善於合作",
+    "shortcoming": "沒有主見"
+  },
+  {
+    "advantage": "維持關係",
+    "shortcoming": "逃避衝突"
+  },
+  {
+    "advantage": "不傷害人",
+    "shortcoming": "隱藏己見"
+  },
+  {
+    "advantage": "謙虛待人",
+    "shortcoming": "缺乏自信"
+  },
+  {
+    "advantage": "不犯大錯",
+    "shortcoming": "糾結小錯"
+  },
+  {
+    "advantage": "聆聽他人",
+    "shortcoming": "過度依賴"
+  },
+  {
+    "advantage": "善於忍耐",
+    "shortcoming": "壓抑自我"
+  },
+  {
+    "advantage": "小心警慎",
+    "shortcoming": "猜疑心重"
+  },
+  {
+    "advantage": "工作有效",
+    "shortcoming": "只顧己利"
+  },
+  {
+    "advantage": "沉著冷靜",
+    "shortcoming": "缺乏幽默"
+  },
+  {
+    "advantage": "做事高標",
+    "shortcoming": "工作狂熱"
+  },
+  {
+    "advantage": "理性行事",
+    "shortcoming": "待人冷漠"
+  },
+  {
+    "advantage": "獨立完成",
+    "shortcoming": "不愛合作"
+  },
+  {
+    "advantage": "充分準備",
+    "shortcoming": "過於執著"
+  },
+  {
+    "advantage": "客觀判斷",
+    "shortcoming": "只重資訊"
+  },
+  {
+    "advantage": "認真分析",
+    "shortcoming": "優柔寡斷"
+  },
+  {
+    "advantage": "應對快速",
+    "shortcoming": "欠缺考量"
+  },
+  {
+    "advantage": "享受改變",
+    "shortcoming": "缺乏一致"
+  },
+  {
+    "advantage": "善於發想",
+    "shortcoming": "做白日夢"
+  },
+  {
+    "advantage": "追求自由",
+    "shortcoming": "不受控制"
+  },
+  {
+    "advantage": "情感豐富",
+    "shortcoming": "情緒難抑"
+  },
+  {
+    "advantage": "好惡分明",
+    "shortcoming": "性格獨特"
+  },
+  {
+    "advantage": "勇於嘗新",
+    "shortcoming": "三分熱度"
+  }
+];
