9e7daa3b87682a35f4bb371e5a6c3d22a6ae1530
Robin <alegalone@gmail.com>
2022-08-07 20:46:43 +0800

    upd. game4Data.js

--- game4Data.js
+++ game4Data.js
@@ -1,182 +1,257 @@
- var data = [
- {
-   "picture": "https://i.imgur.com/Aqc4DWR.jpg",
-   "advantage": "自信表達",
-   "shortcoming": "堅持己見"
- },
- {
-   "picture": "https://i.imgur.com/nS9l8G0.jpg",
-   "advantage": "貫徹始終",
-   "shortcoming": "缺乏應變"
- },
- {
-   "picture": "https://i.imgur.com/8PdaqbG.jpg",
-   "advantage": "責任感強",
-   "shortcoming": "工作過勞"
- },
- {
-   "picture": "https://i.imgur.com/bu9ZSZf.jpg",
-   "advantage": "善於改進",
-   "shortcoming": "喜愛批判"
- },
- {
-   "picture": "https://i.imgur.com/XPx62hk.jpg",
-   "advantage": "懂得自制",
-   "shortcoming": "支配慾高"
- },
- {
-   "picture": "https://i.imgur.com/Ce6fmDI.jpg",
-   "advantage": "擅長整理",
-   "shortcoming": "難忍雜亂"
- },
- {
-   "picture": "https://i.imgur.com/jfFaSPr.jpg",
-   "advantage": "同理他人",
-   "shortcoming": "過度大方"
- },
- {
-   "picture": "https://i.imgur.com/7yeNIml.jpg",
-   "advantage": "待人親切",
-   "shortcoming": "費心討好"
- },
- {
-   "picture": "https://i.imgur.com/PNKC47m.jpg",
-   "advantage": "炒熱氣氛",
-   "shortcoming": "難忍尷尬"
- },
- {
-   "picture": "https://i.imgur.com/gergCpT.jpg",
-   "advantage": "善於讚賞",
-   "shortcoming": "不說真相"
- },
- {
-   "picture": "https://i.imgur.com/OMaPcxQ.jpg",
-   "advantage": "以人為重",
-   "shortcoming": "容易吃虧"
- },
- {
-   "picture": "https://i.imgur.com/GwYUEwR.jpg",
-   "advantage": "樂於助人",
-   "shortcoming": "不會拒絕"
- },
- {
-   "picture": "https://i.imgur.com/E3e69w0.jpg",
-   "advantage": "待人溫柔",
-   "shortcoming": "內向害羞"
- },
- {
-   "picture": "https://i.imgur.com/h9KSGfp.jpg",
-   "advantage": "善於合作",
-   "shortcoming": "沒有主見"
- },
- {
-   "picture": "https://i.imgur.com/nteaAlr.jpg",
-   "advantage": "維持關係",
-   "shortcoming": "逃避衝突"
- },
- {
-   "picture": "https://i.imgur.com/Vu46Mip.jpg",
-   "advantage": "不傷害人",
-   "shortcoming": "隱藏己見"
- },
- {
-   "picture": "https://i.imgur.com/3I3OQZ6.jpg",
-   "advantage": "謙虛待人",
-   "shortcoming": "缺乏自信"
- },
- {
-   "picture": "https://i.imgur.com/Txwa2e1.jpg",
-   "advantage": "不犯大錯",
-   "shortcoming": "糾結小錯"
- },
- {
-   "picture": "https://i.imgur.com/u6TvayJ.jpg",
-   "advantage": "聆聽他人",
-   "shortcoming": "過度依賴"
- },
- {
-   "picture": "https://i.imgur.com/QxEQF1m.jpg",
-   "advantage": "善於忍耐",
-   "shortcoming": "壓抑自我"
- },
- {
-   "picture": "https://i.imgur.com/oVvgSd1.jpg",
-   "advantage": "小心警慎",
-   "shortcoming": "猜疑心重"
- },
- {
-   "picture": "https://i.imgur.com/VKHziSc.jpg",
-   "advantage": "工作有效",
-   "shortcoming": "只顧己利"
- },
- {
-   "picture": "https://i.imgur.com/IfOOYdX.jpg",
-   "advantage": "沉著冷靜",
-   "shortcoming": "缺乏幽默"
- },
- {
-   "picture": "https://i.imgur.com/gJLmj7a.jpg",
-   "advantage": "做事高標",
-   "shortcoming": "工作狂熱"
- },
- {
-   "picture": "https://i.imgur.com/iaSxaKd.jpg",
-   "advantage": "理性行事",
-   "shortcoming": "待人冷漠"
- },
- {
-   "picture": "https://i.imgur.com/pPb238v.jpg",
-   "advantage": "獨立完成",
-   "shortcoming": "不愛合作"
- },
- {
-   "picture": "https://i.imgur.com/wDZrKCt.jpg",
-   "advantage": "充分準備",
-   "shortcoming": "過於執著"
- },
- {
-   "picture": "https://i.imgur.com/eLTLWb9.jpg",
-   "advantage": "客觀判斷",
-   "shortcoming": "只重資訊"
- },
- {
-   "picture": "https://i.imgur.com/LZ5xRhB.jpg",
-   "advantage": "認真分析",
-   "shortcoming": "優柔寡斷"
- },
- {
-   "picture": "https://i.imgur.com/QfIaDSz.jpg",
-   "advantage": "應對快速",
-   "shortcoming": "欠缺考量"
- },
- {
-   "picture": "https://i.imgur.com/3k0x7QQ.jpg",
-   "advantage": "享受改變",
-   "shortcoming": "缺乏一致"
- },
- {
-   "picture": "https://i.imgur.com/pDuuQ0p.jpg",
-   "advantage": "善於發想",
-   "shortcoming": "做白日夢"
- },
- {
-   "picture": "https://i.imgur.com/JGL2nRt.jpg",
-   "advantage": "追求自由",
-   "shortcoming": "不受控制"
- },
- {
-   "picture": "https://i.imgur.com/iaHZ1pS.jpg",
-   "advantage": "情感豐富",
-   "shortcoming": "情緒難抑"
- },
- {
-   "picture": "https://i.imgur.com/AbbQlDZ.jpg",
-   "advantage": "好惡分明",
-   "shortcoming": "性格獨特"
- },
- {
-   "picture": "https://i.imgur.com/0JJbYeX.jpg",
-   "advantage": "勇於嘗新",
-   "shortcoming": "三分熱度"
- }
-]:
+ var data = {
+   imgs: [{
+       "picture": "https://i.imgur.com/Aqc4DWR.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/nS9l8G0.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/8PdaqbG.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/bu9ZSZf.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/XPx62hk.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/Ce6fmDI.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/jfFaSPr.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/7yeNIml.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/PNKC47m.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/gergCpT.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/OMaPcxQ.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/GwYUEwR.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/E3e69w0.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/h9KSGfp.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/nteaAlr.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/Vu46Mip.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/3I3OQZ6.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/Txwa2e1.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/u6TvayJ.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/QxEQF1m.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/oVvgSd1.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/VKHziSc.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/IfOOYdX.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/gJLmj7a.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/iaSxaKd.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/pPb238v.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/wDZrKCt.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/eLTLWb9.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/LZ5xRhB.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/QfIaDSz.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/3k0x7QQ.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/pDuuQ0p.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/JGL2nRt.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/iaHZ1pS.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/AbbQlDZ.jpg"
+     },
+     {
+       "picture": "https://i.imgur.com/0JJbYeX.jpg"
+     }
+   ],
+   character: [{
+       "advantage": "自信表達",
+       "shortcoming": "堅持己見"
+     },
+     {
+       "advantage": "貫徹始終",
+       "shortcoming": "缺乏應變"
+     },
+     {
+       "advantage": "責任感強",
+       "shortcoming": "工作過勞"
+     },
+     {
+       "advantage": "善於改進",
+       "shortcoming": "喜愛批判"
+     },
+     {
+       "advantage": "懂得自制",
+       "shortcoming": "支配慾高"
+     },
+     {
+       "advantage": "擅長整理",
+       "shortcoming": "難忍雜亂"
+     },
+     {
+       "advantage": "同理他人",
+       "shortcoming": "過度大方"
+     },
+     {
+       "advantage": "待人親切",
+       "shortcoming": "費心討好"
+     },
+     {
+       "advantage": "炒熱氣氛",
+       "shortcoming": "難忍尷尬"
+     },
+     {
+       "advantage": "善於讚賞",
+       "shortcoming": "不說真相"
+     },
+     {
+       "advantage": "以人為重",
+       "shortcoming": "容易吃虧"
+     },
+     {
+       "advantage": "樂於助人",
+       "shortcoming": "不會拒絕"
+     },
+     {
+       "advantage": "待人溫柔",
+       "shortcoming": "內向害羞"
+     },
+     {
+       "advantage": "善於合作",
+       "shortcoming": "沒有主見"
+     },
+     {
+       "advantage": "維持關係",
+       "shortcoming": "逃避衝突"
+     },
+     {
+       "advantage": "不傷害人",
+       "shortcoming": "隱藏己見"
+     },
+     {
+       "advantage": "謙虛待人",
+       "shortcoming": "缺乏自信"
+     },
+     {
+       "advantage": "不犯大錯",
+       "shortcoming": "糾結小錯"
+     },
+     {
+       "advantage": "聆聽他人",
+       "shortcoming": "過度依賴"
+     },
+     {
+       "advantage": "善於忍耐",
+       "shortcoming": "壓抑自我"
+     },
+     {
+       "advantage": "小心警慎",
+       "shortcoming": "猜疑心重"
+     },
+     {
+       "advantage": "工作有效",
+       "shortcoming": "只顧己利"
+     },
+     {
+       "advantage": "沉著冷靜",
+       "shortcoming": "缺乏幽默"
+     },
+     {
+       "advantage": "做事高標",
+       "shortcoming": "工作狂熱"
+     },
+     {
+       "advantage": "理性行事",
+       "shortcoming": "待人冷漠"
+     },
+     {
+       "advantage": "獨立完成",
+       "shortcoming": "不愛合作"
+     },
+     {
+       "advantage": "充分準備",
+       "shortcoming": "過於執著"
+     },
+     {
+       "advantage": "客觀判斷",
+       "shortcoming": "只重資訊"
+     },
+     {
+       "advantage": "認真分析",
+       "shortcoming": "優柔寡斷"
+     },
+     {
+       "advantage": "應對快速",
+       "shortcoming": "欠缺考量"
+     },
+     {
+       "advantage": "享受改變",
+       "shortcoming": "缺乏一致"
+     },
+     {
+       "advantage": "善於發想",
+       "shortcoming": "做白日夢"
+     },
+     {
+       "advantage": "追求自由",
+       "shortcoming": "不受控制"
+     },
+     {
+       "advantage": "情感豐富",
+       "shortcoming": "情緒難抑"
+     },
+     {
+       "advantage": "好惡分明",
+       "shortcoming": "性格獨特"
+     },
+     {
+       "advantage": "勇於嘗新",
+       "shortcoming": "三分熱度"
+     }
+   ]
+
+ };
